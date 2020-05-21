6965e47d060a4225a373937aae1bdc3a47f8bd50
kai4idps <kai@4idps.com>
2020-05-21 18:31:55 +0800

    form

--- src/container/Checkout/ContactData/ContactData.js
+++ src/container/Checkout/ContactData/ContactData.js
@@ -2,15 +2,57 @@ import React, { useState } from "react"
 import Button from "../../../component/UI/Button/Button"
 import axios from "../../../axiosOrder"
 import Spinner from "../../../component/UI/spinner/Spinner"
-
+import Input from "../../../component/UI/Input/Input"
 import classes from "./ContactData.module.css"
 
 const ContactData = props => {
   console.log(props)
   console.log(props.ingredients)
 
-  const formData = { name: "", email: "", age: "" }
+  const formData = {
+    name: {
+      elementType: "input",
+      elementConfig: {
+        type: "text",
+        placeholder: "Your Name"
+      },
+      value: "",
+      validation: {
+        required: true
+      },
+      valid: false,
+      touched: false
+    },
+    email: {
+      elementType: "input",
+      elementConfig: {
+        type: "email",
+        placeholder: "Your E-Mail"
+      },
+      value: "",
+      validation: {
+        required: true,
+        isEmail: true
+      },
+      valid: false,
+      touched: false
+    },
+    deliveryMethod: {
+      elementType: "select",
+      elementConfig: {
+        options: [
+          { value: "fastest", displayValue: "Fastest" },
+          { value: "cheapest", displayValue: "Cheapest" }
+        ]
+      },
+      value: "",
+      validation: {},
+      valid: true
+    }
+  }
+  const [orderForm, setOrderForm] = useState(formData)
   const [loading, setLoading] = useState(false)
+  const [formIsValid, setFormIsValid] = useState(false)
   const ingredient = props.ingredients
   const price = props.price
 
@@ -18,14 +60,15 @@ const ContactData = props => {
     event.preventDefault()
 
     setLoading(true)
+
+    const formData = {}
+    for (let formElementIdentifier in orderForm) {
+      formData[formElementIdentifier] = orderForm[formElementIdentifier].value
+    }
     const order = {
       ingredients: ingredient,
       price: price,
-      customer: {
-        name: "LEE",
-        email: "test @gmail.com",
-        deliveryMethod: "fatest"
-      }
+      orderData: formData
     }
     console.log(order)
 
@@ -40,28 +83,90 @@ const ContactData = props => {
         console.log(error)
       })
   }
+  //inputChange
+  const inputChangedHandler = (event, inputIdentifier) => {
+    console.log(event.target.value)
 
+    const updatedOrderForm = {
+      ...orderForm
+    }
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    }
+    updatedFormElement.value = event.target.value
+    updatedFormElement.valid = checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    )
+    updatedFormElement.touched = true
+    updatedOrderForm[inputIdentifier] = updatedFormElement
+
+    let formIsValid = true
+    //以驗證過正確格式
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid
+      console.log(updatedOrderForm)
+    }
+    setOrderForm(updatedOrderForm)
+    setFormIsValid(formIsValid)
+    // this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid })
+  }
+  //檢查欄位
+  const checkValidity = (value, rules) => {
+    console.log(rules)
+
+    let isValid = true
+    if (!rules) {
+      return true
+    }
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid
+    }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+      isValid = pattern.test(value) && isValid
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/
+      isValid = pattern.test(value) && isValid
+    }
+
+    return isValid
+  }
+  //動態產生表單
+  const formElementsArray = []
+  for (let key in orderForm) {
+    formElementsArray.push({
+      id: key,
+      config: orderForm[key]
+    })
+  }
   let form = (
-    <form>
-      <input
-        className={classes.Input}
-        type="text"
-        name="name"
-        placeholder="name"
-      />
-      <input
-        className={classes.Input}
-        type="email"
-        name="email"
-        placeholder="email"
-      />
-      <input
-        className={classes.Input}
-        type="number"
-        name="age"
-        placeholder="age"
-      />
-      <Button btnType="Success" clicked={orderHandler}>
+    <form onSubmit={orderHandler}>
+      {formElementsArray.map(formElement => (
+        <Input
+          key={formElement.id}
+          elementType={formElement.config.elementType}
+          elementConfig={formElement.config.elementConfig}
+          value={formElement.config.value}
+          invalid={!formElement.config.valid}
+          shouldValidate={formElement.config.validation}
+          touched={formElement.config.touched}
+          changed={event => inputChangedHandler(event, formElement.id)}
+        />
+      ))}
+      <Button btnType="Success" disabled={!formIsValid}>
         ORDER
       </Button>
     </form>
